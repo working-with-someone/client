e856add75f24e81f9d3694c234909c47ccc4af04
seungho-hub <kmc54320@gmail.com>
2024-08-22 19:31:16 +0900

    add : load user data root layout

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,18 @@
+import { redirect, type Load } from "@sveltejs/kit";
+
+export const load: Load = async ({ fetch }) => {
+    const getUserSelfURL = new URL('/v1/users/self', import.meta.env.VITE_API_SERVER_DOMAIN);
+
+    const res = await fetch(getUserSelfURL);
+
+    const data = await res.json();
+
+    if (res.status == 401) {
+        const redirectURL = new URL('/', import.meta.env.VITE_CLIENT_SERVER_DOMAIN);
+        const loginURL = new URL("/auth/login", import.meta.env.VITE_AUTH_SERVER_DOMAIN);
+        loginURL.searchParams.append("continue", redirectURL.toString());
+        redirect(302, loginURL);
+    }
+
+    return { user: data.user };
+};
