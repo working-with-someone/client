5251629d26606b0bdac4713d60ab6994690884dc
seungho-hub <kmc54320@gmail.com>
2024-10-18 19:16:26 +0900

    add user not found handling

--- src/routes/user/[userId]/+page.server.ts
+++ src/routes/user/[userId]/+page.server.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_API_SERVER_DOMAIN } from "$env/static/public";
 import type { Load } from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
 
 export const load: Load = async ({fetch, params}) => {
   const getUserEndpoint = new URL(`/users/${params.userId}`, PUBLIC_API_SERVER_DOMAIN);
@@ -8,8 +9,13 @@ export const load: Load = async ({fetch, params}) => {
 
   const targetUser = await res.json();
 
+  if(targetUser.status == 404){
+		error(404, {
+			message: '사용자를 찾을 수 없습니다',
+		});
+  }
+
   return {
     targetUser
-    
   };
 };
