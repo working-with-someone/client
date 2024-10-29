417466b703756ab230883a535fe4aaeb9aada62b
seungho-hub <kmc54320@gmail.com>
2024-10-29 13:25:24 +0900

    add : attach userId to locals of request object

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,6 @@ import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { PUBLIC_API_SERVER_DOMAIN, PUBLIC_AUTH_SERVER_DOMAIN, PUBLIC_CLIENT_SERVER_DOMAIN } from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
-    return await resolve(event);
-}
-
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
     // api server에 전송하는 fetch request라면 cookie를 포함시킨다.
     if (request.url.startsWith(PUBLIC_API_SERVER_DOMAIN)) {
@@ -23,4 +19,16 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
     }
 
     return res;
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const getSelfEndpoint = new URL('/users/self', PUBLIC_API_SERVER_DOMAIN);
+
+    const getSelfRes = await fetch(getSelfEndpoint)
+
+    const userSelf = await getSelfRes.json();
+
+    event.locals.userId = userSelf.id;
+
+    return await resolve(event);
 }
