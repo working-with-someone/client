a6af4fa8fd1edeb103794022f031c66f1669ddff
seungho-hub <kmc54320@gmail.com>
2024-11-01 00:39:28 +0900

    add session page

---
+++ src/routes/session/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let data;
+	import SessionCard from '$lib/components/video/sessionCard.svelte';
+</script>
+
+<section id="sessions">
+	<div class="deck">
+		{#each data.sessions as session}
+			<SessionCard {session} />
+		{/each}
+	</div>
+</section>
+
+<style lang="scss">
+	section#sessions {
+		.deck {
+			display: flex;
+			flex-direction: row;
+			gap: 20px;
+		}
+	}
+</style>
