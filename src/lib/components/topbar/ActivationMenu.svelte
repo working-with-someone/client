09306da806d98ce9c814e242c2d1560193a890ce
seungho-hub <kmc54320@gmail.com>
2024-11-02 01:50:01 +0900

    add activation menus

---
+++ src/lib/components/topbar/ActivationMenu.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<div class="activation-menus">
+	<button class="menu-item open-session">
+		<span class="material-symbols-outlined"> radio_button_checked </span>
+	</button>
+
+	<button class="menu-item upload-session">
+		<span class="material-symbols-outlined"> video_call </span>
+	</button>
+</div>
+
+<style lang="scss">
+	.activation-menus {
+		.menu-item.open-session{
+			&:hover {
+				span {
+					color: var(--font-red);
+				}
+			}
+		}
+
+    .menu-item.upload-session{
+      &:hover {
+        span {
+          color: var(--sig);
+        }
+      }
+    }
+	}
+</style>
