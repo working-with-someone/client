770b7ededd160ed43a2e54f8d56278cba8cc270a
seungho-hub <kmc54320@gmail.com>
2024-11-02 01:50:39 +0900

    update topbar

--- src/lib/components/topbar/TopBar.svelte
+++ src/lib/components/topbar/TopBar.svelte
@@ -1,13 +1,39 @@
 <script>
+	import SearchBar from "./SearchBar.svelte";
+	import ActivationMenu from "./ActivationMenu.svelte";
 </script>
 
-<section id="top-bar">topbar</section>
+<section id="top-bar">
+	<div class="start">
+
+	</div>
+	<div class="center">
+		<SearchBar />
+	</div>
+	<div class="end">
+		<ActivationMenu />
+	</div>
+
+</section>
 
 <style lang="scss">
 	section#top-bar {
-		grid-area: topbar;
 		background-color: var(--bg);
-
 		-webkit-app-region: drag;
+
+		display : flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom : 20px;
+		
+		.start{
+			width : 200px;
+		}
+		.end {
+			width : 200px;
+			display : flex;
+			justify-content: end;
+		}
+		
 	}
 </style>
