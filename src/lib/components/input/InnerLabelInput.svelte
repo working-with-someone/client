d9106f825b7ed678a8a5d2590caf1f52aebedd8f
seungho-hub <kmc54320@gmail.com>
2024-11-05 21:28:47 +0900

    add inner label input component

---
+++ src/lib/components/input/InnerLabelInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let label;
+</script>
+
+<div class="inner-label-input middle-rounded">
+  <span>{label}</span>
+  <slot></slot>
+</div>
+
+<style lang="scss">
+  .inner-label-input{
+    padding : 4px 6px;
+    display : flex;
+    flex-direction: column;
+    gap : 5px;
+    border : 1px solid var(--font-light-gray);
+
+    &:hover{
+      border : 1px solid var(--font-white);
+    }
+    span{
+      font-size : 12px;
+      padding : 0 4px;
+      color : gray;
+    }
+  }
+</style>
