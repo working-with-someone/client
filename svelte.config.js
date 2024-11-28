f79d0a3139deb9e66027f9a4120d0f30b36b39d4
seungho-hub <kmc54320@gmail.com>
2024-11-28 14:38:34 +0900

    enable csp only in production mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const cspConfig = {
+	directives : {
+		"default-src": ["self", "https://*.wws.com"],
+		"style-src": ["https://fonts.googleapis.com"],
+		"font-src": ["https://fonts.gstatic.com"],
+		"img-src": ["self", "https://*.wws.com", "data:", "*"],
+	}
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,15 +20,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		csp: {
-      directives: {
-        "default-src": ["self", "https://*.wws.com"],
-        "style-src": ["https://fonts.googleapis.com"],
-        "font-src": ["https://fonts.gstatic.com"],
-        "img-src": ["self", "https://*.wws.com", "data:", "*"],
-      },
-    },
+		adapter: adapter({
+			out : "build",
+			
+		}),
+		csp: process.env.NODE_ENV == "production" ? cspConfig : undefined
+
 	},
 	onwarn: (warning, handler) => {
     if (warning.code.startsWith("a11y-")) return;
