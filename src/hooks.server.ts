2cc548b06f1e63caa9212b18b1665f7df703b2c6
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-12-11 20:30:41 +0900

    rename continue query parameter to continue_uri for sync auth server update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,33 +1,37 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
-import { PUBLIC_API_SERVER_DOMAIN, PUBLIC_AUTH_SERVER_DOMAIN, PUBLIC_CLIENT_SERVER_DOMAIN } from '$env/static/public';
+import {
+	PUBLIC_API_SERVER_DOMAIN,
+	PUBLIC_AUTH_SERVER_DOMAIN,
+	PUBLIC_CLIENT_SERVER_DOMAIN
+} from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-    // api server에 전송하는 fetch request라면 cookie를 포함시킨다.
-    if (request.url.startsWith(PUBLIC_API_SERVER_DOMAIN)) {
-        request.headers.set('cookie', event.request.headers.get('cookie') as string);
-    }
-    
-    const res = await fetch(request);
+	// api server에 전송하는 fetch request라면 cookie를 포함시킨다.
+	if (request.url.startsWith(PUBLIC_API_SERVER_DOMAIN)) {
+		request.headers.set('cookie', event.request.headers.get('cookie') as string);
+	}
 
-    if(res.status == 401){
-        const redirectURL = new URL('/', PUBLIC_CLIENT_SERVER_DOMAIN);
-        const loginURL = new URL("/auth/login", PUBLIC_AUTH_SERVER_DOMAIN);
-        loginURL.searchParams.append("continue", redirectURL.toString());
+	const res = await fetch(request);
 
-        redirect(302, loginURL);
-    }
+	if (res.status == 401) {
+		const redirectURL = new URL('/', PUBLIC_CLIENT_SERVER_DOMAIN);
+		const loginURL = new URL('/auth/login', PUBLIC_AUTH_SERVER_DOMAIN);
+		loginURL.searchParams.append('continue_uri', redirectURL.toString());
 
-    return res;
-}
+		redirect(302, loginURL);
+	}
+
+	return res;
+};
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const getSelfEndpoint = new URL('/users/self', PUBLIC_API_SERVER_DOMAIN);
-    const getSelfRes = await event.fetch(getSelfEndpoint)
+	const getSelfEndpoint = new URL('/users/self', PUBLIC_API_SERVER_DOMAIN);
+	const getSelfRes = await event.fetch(getSelfEndpoint);
 
-    const userSelf = await getSelfRes.json();
+	const userSelf = await getSelfRes.json();
 
-    event.locals.userId = userSelf.id;
+	event.locals.userId = userSelf.id;
 
-    return await resolve(event);
-}
+	return await resolve(event);
+};
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,24 @@
-import { PUBLIC_API_SERVER_DOMAIN, PUBLIC_AUTH_SERVER_DOMAIN, PUBLIC_CLIENT_SERVER_DOMAIN } from "$env/static/public";
-import { redirect, type Load } from "@sveltejs/kit";
+import {
+	PUBLIC_API_SERVER_DOMAIN,
+	PUBLIC_AUTH_SERVER_DOMAIN,
+	PUBLIC_CLIENT_SERVER_DOMAIN
+} from '$env/static/public';
+import { redirect, type Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ fetch }) => {
-    const getUserSelfURL = new URL('/users/self', PUBLIC_API_SERVER_DOMAIN);
+	const getUserSelfURL = new URL('/users/self', PUBLIC_API_SERVER_DOMAIN);
 
-    const res = await fetch(getUserSelfURL);
+	const res = await fetch(getUserSelfURL);
 
-    const data = await res.json();
+	if (res.status == 401) {
+		const redirectURL = new URL('/', PUBLIC_CLIENT_SERVER_DOMAIN);
+		const loginURL = new URL('/auth/login', PUBLIC_AUTH_SERVER_DOMAIN);
+		loginURL.searchParams.append('continue_uri', redirectURL.toString());
 
-    if (res.status == 401) {
-        const redirectURL = new URL('/', PUBLIC_CLIENT_SERVER_DOMAIN);
-        const loginURL = new URL("/auth/login", PUBLIC_AUTH_SERVER_DOMAIN);
-        loginURL.searchParams.append("continue", redirectURL.toString());
+		redirect(302, loginURL);
+	}
 
-        redirect(302, loginURL);
-    }
+	const data = await res.json();
 
-    return { user: data };
+	return { user: data };
 };
