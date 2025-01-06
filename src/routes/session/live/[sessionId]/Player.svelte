1d1d66ff23922af5ef6f120cce6cd379a2607b86
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-01-06 22:04:47 +0900

    add livession player

---
+++ src/routes/session/live/[sessionId]/Player.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	interface Props {
+		mediaStream?: MediaStream;
+	}
+
+	let { mediaStream }: Props = $props();
+
+	let video: HTMLVideoElement;
+
+	$effect(() => {
+		if (mediaStream) {
+			video.srcObject = mediaStream;
+		}
+	});
+</script>
+
+<div id="player">
+	<video src="" bind:this={video} autoplay={true}></video>
+	<div class="progress"></div>
+</div>
+
+<style lang="scss">
+	#player {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		background-color: var(--bg);
+		video {
+			width: 100%;
+			max-height: 785px;
+		}
+
+		.progress {
+			width: 100%;
+			height: 5px;
+			background-color: var(--sig);
+		}
+	}
+</style>
