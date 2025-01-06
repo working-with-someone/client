1bda7b115b5eb65d94f054026e095420344d0772
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-01-06 15:28:21 +0900

    remove wrapper

--- src/routes/session/live/[sessionId]/+page.svelte
+++ src/routes/session/live/[sessionId]/+page.svelte
@@ -16,24 +16,16 @@
 
 <section id="live-session">
 	{#if liveSessionManager.isReady}
-		<div class="live-session-open-config">
-			<LiveSessionOpenConfig {liveSessionManager} {mediaController} />
-		</div>
-	{:else if liveSessionManager.isOpened}
-		<div class="session">session</div>
-	{/if}
+		<LiveSessionOpenConfig {liveSessionManager} {mediaController} />
+	{:else if liveSessionManager.isOpened}{/if}
 </section>
 
 <style lang="scss">
 	section#live-session {
 		width: 100%;
 		height: 100%;
-		div.live-session-open-config {
-			width: 100%;
-			height: 100%;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-		}
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
