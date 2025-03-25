6acac8cc6469bfde12f6df0a522f79bf61ab49c1
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-03-25 21:23:10 +0900

    add text error component

---
+++ src/lib/components/error/TextError.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		error?: App.Error;
+	}
+
+	const { error }: Props = $props();
+</script>
+
+{#if error}
+	<span> {error.message} </span>
+{/if}
+
+<style lang="scss">
+	span {
+		color: var(--mid-red);
+	}
+</style>
