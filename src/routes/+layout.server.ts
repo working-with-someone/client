ade121989c144e98cda54c7f1a3250659d06d728
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-04-21 23:59:55 +0900

    remove duplicate redirect not authenticated user logic

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,21 +1,11 @@
-import { PUBLIC_AUTH_SERVER_DOMAIN, PUBLIC_CLIENT_SERVER_DOMAIN } from '$env/static/public';
-
 import { PRIVATE_API_SERVER_DOMAIN } from '$env/static/private';
 
-import { redirect, type Load } from '@sveltejs/kit';
+import { type Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ fetch }) => {
 	const getUserSelfURL = new URL('/users/self', PRIVATE_API_SERVER_DOMAIN);
 
 	const res = await fetch(getUserSelfURL);
-
-	if (res.status == 401) {
-		const redirectURL = new URL('/', PUBLIC_CLIENT_SERVER_DOMAIN);
-		const loginURL = new URL('/auth/login', PUBLIC_AUTH_SERVER_DOMAIN);
-		loginURL.searchParams.append('continue_uri', redirectURL.toString());
-
-		redirect(302, loginURL);
-	}
 
 	const body = await res.json();
 	const user = body.data;
