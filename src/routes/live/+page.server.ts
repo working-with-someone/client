8f76070f2d420b3589cdbb46fe4a4b1d80544695
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2026-01-15 00:01:08 +0900

    update : live session page must load opened, breaked live session only

--- src/routes/live/+page.server.ts
+++ src/routes/live/+page.server.ts
@@ -1,7 +1,7 @@
 import type { Load } from '@sveltejs/kit';
 import { PRIVATE_API_SERVER_DOMAIN } from '$env/static/private';
-import type { category } from '@prisma/client';
-import type { LiveSession } from '../../types/session';
+import { live_session_status, type category } from '@prisma/client';
+import type { LiveSessionWithAll } from '../../types/session';
 
 export const load: Load = async ({ fetch }) => {
 	const getCategoriesEndpointUrl = new URL(`/categories`, PRIVATE_API_SERVER_DOMAIN);
@@ -14,17 +14,19 @@ export const load: Load = async ({ fetch }) => {
 
 	const majorCategories = (await getCategoriesRes.json()).data as category[];
 
-	const categorizedLiveSessionsList = new Map<string, LiveSession[]>();
+	const categorizedLiveSessionsList = new Map<string, LiveSessionWithAll[]>();
 
 	for (const category of majorCategories) {
 		const getLiveSessionsEndpointUrl = new URL(`/sessions/live`, PRIVATE_API_SERVER_DOMAIN);
 		getLiveSessionsEndpointUrl.searchParams.set('category', category.label);
+		getLiveSessionsEndpointUrl.searchParams.append('status', live_session_status.OPENED);
+		getLiveSessionsEndpointUrl.searchParams.append('status', live_session_status.BREAKED);
 		getLiveSessionsEndpointUrl.searchParams.set('page', '1');
 		getLiveSessionsEndpointUrl.searchParams.set('per_page', '10');
 
 		const getLiveSessionsRes = await fetch(getLiveSessionsEndpointUrl);
 
-		const liveSessions = (await getLiveSessionsRes.json()).data as LiveSession[];
+		const liveSessions = (await getLiveSessionsRes.json()).data as LiveSessionWithAll[];
 
 		categorizedLiveSessionsList.set(category.label, liveSessions);
 	}
