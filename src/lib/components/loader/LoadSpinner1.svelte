518a7c0e0d99b19311b4f9ee5e350680fe55f7bc
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2026-05-27 03:41:12 +0900

    add new load spinner

---
+++ src/lib/components/loader/LoadSpinner1.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	interface Props {
+		size: number;
+	}
+
+	const { size } = $props();
+</script>
+
+<span class="loader" style="width : {size}px; height : {size}px;"></span>
+
+<style lang="scss">
+	.loader {
+		border-radius: 50%;
+		display: inline-block;
+		border-top: 3px solid #fff;
+		border-right: 3px solid transparent;
+		box-sizing: border-box;
+		animation: rotation 1s linear infinite;
+	}
+
+	@keyframes rotation {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
