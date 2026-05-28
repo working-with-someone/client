fb55e91a81d204928e943a0a601abbe5f3922da2
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2026-05-28 19:23:38 +0900

    fix: correct video URL path in media server configuration

--- src/config/path.config.svelte.ts
+++ src/config/path.config.svelte.ts
@@ -3,7 +3,7 @@ import { PUBLIC_MEDIA_STATIC_SERVER_DOMAIN } from '$env/static/public';
 export const to = {
 	mediaServer: {
 		staticServer: {
-			video: new URL('media/video', PUBLIC_MEDIA_STATIC_SERVER_DOMAIN).href
+			video: new URL('media/video/', PUBLIC_MEDIA_STATIC_SERVER_DOMAIN).href
 		}
 	}
 };
