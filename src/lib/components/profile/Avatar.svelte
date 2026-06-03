eef8e536906b51bc8710389348688fdee72ad236
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2026-06-04 00:00:42 +0900

    refactor : componentize pfp

--- src/lib/components/profile/Avatar.svelte
+++ src/lib/components/profile/Avatar.svelte
@@ -1,8 +1,8 @@
 <script>
-	import { PUBLIC_API_SERVER_DOMAIN } from '$env/static/public';
 	import Modal from '../modals/Modal.svelte';
 	import UserConfig from '../configuration/ProfileConfig.svelte';
 	import { getContext } from 'svelte';
+	import Pfp from '../user/pfp.svelte';
 
 	const user = getContext('user');
 
@@ -18,12 +18,7 @@
 </script>
 
 <button class="avatar" onclick={openUserConfigModal} aria-hidden="true">
-	<div class="pfp">
-		<img
-			src={`${PUBLIC_API_SERVER_DOMAIN}${$user.pfp.curr}`}
-			alt={`${PUBLIC_API_SERVER_DOMAIN}/media/images/default/pfp`}
-		/>
-	</div>
+	<Pfp pfpUri={$user.pfp.curr} size={46} />
 </button>
 
 {#if showUserConfigModal}
@@ -39,19 +34,6 @@
 		justify-content: center;
 		&:hover {
 			cursor: pointer;
-		}
-
-		.pfp {
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			width: 46px;
-			height: 46px;
-			border-radius: 23px;
-			overflow: hidden;
-			img {
-				width: 100%;
-			}
 		}
 	}
 </style>
---
+++ src/lib/components/user/pfp.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { PUBLIC_API_SERVER_DOMAIN } from '$env/static/public';
+
+	interface Props {
+		pfpUri: string;
+		size?: number;
+	}
+
+	const { pfpUri, size = 50 }: Props = $props();
+</script>
+
+<img
+	src={`${PUBLIC_API_SERVER_DOMAIN}${pfpUri}`}
+	alt={`${PUBLIC_API_SERVER_DOMAIN}/media/images/default/pfp`}
+	style={`width: ${size}px; height: ${size}px;`}
+/>
+
+<style lang="scss">
+	img {
+		border-radius: 50%;
+		object-fit: cover;
+	}
+</style>
